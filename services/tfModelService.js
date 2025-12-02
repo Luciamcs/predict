8d5855e0cafffb04d548c4729455638bfcc2eedf
Luciamcs <lmc146@alu.ua.es>
2025-12-02 10:44:09 +0100

    cambio en error del postman

--- services/tfModelService.js
+++ services/tfModelService.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { pathToFileURL } = require("url");
 const tf = require("@tensorflow/tfjs");
 const wasmBackend = require("@tensorflow/tfjs-backend-wasm");
-const MODEL_VERSION= process.env.MONGO_URI || "v1.0";
+const MODEL_VERSION= process.env.MONGO_VERSION || "v1.0";
 
 
 let model = null;
